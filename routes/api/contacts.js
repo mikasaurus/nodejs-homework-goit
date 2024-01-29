d4fccc2301368e131560189758a84d6e0e2ec5f6
mika <dominikanajda@gmail.com>
2024-01-29 20:43:13 +0100

    mongodb hw submit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,20 @@ import express from "express";
 import * as contacts from "../../models/contacts.js";
 import Joi from "joi";
 
-const router = express.Router();
+const contactsRouter = express.Router();
 
-router.get("/", async (req, res, next) => {
+contactsRouter.get("/", async (req, res, next) => {
   const contactList = await contacts.listContacts();
-  res.status(200).json(contactList);
+  return res.status(200).json(contactList);
 });
 
-router.get("/:contactId", async (req, res, next) => {
+contactsRouter.get("/:contactId", async (req, res, next) => {
   const { id } = req.params;
   const contact = await contacts.getContactById(id);
   if (contact) {
-    res.status(200).json(contact);
+    return res.status(200).json(contact);
   } else {
-    res.status(404).json({ message: "404 Not found!" });
+    return res.status(404).json({ message: "404 Not found!" });
   }
 });
 
@@ -25,24 +25,24 @@ const schemaPost = Joi.object({
   phone: Joi.string().required,
 });
 
-router.post("/", async (req, res, next) => {
+contactsRouter.post("/", async (req, res, next) => {
   const newContact = req.body;
   const createContact = await contacts.addContact(newContact);
   const error = schemaPost.validate(newContact);
   if (error) {
-    res.status(400).json({ message: "Missing required name - field" });
+    return res.status(400).json({ message: "Missing required name - field" });
   } else {
-    res.status(201).json(createContact);
+    return res.status(201).json(createContact);
   }
 });
 
-router.delete("/:contactId", async (req, res, next) => {
+contactsRouter.delete("/:contactId", async (req, res, next) => {
   const { id } = req.params;
   const deleteContact = await contacts.removeContact(id);
   if (deleteContact) {
-    res.status(200).json({ message: "Contact deleted" });
+    return res.status(200).json({ message: "Contact deleted" });
   } else {
-    res.status(404).json({ message: "404 Not found!" });
+    return res.status(404).json({ message: "404 Not found!" });
   }
 });
 
@@ -52,19 +52,30 @@ const schemaPut = Joi.object({
   phone: Joi.string(),
 });
 
-router.put("/:contactId", async (req, res, next) => {
+contactsRouter.put("/:contactId", async (req, res, next) => {
   const { id } = req.params;
   const refreshContact = req.body;
   const error = schemaPut.validate(refreshContact);
   if (error) {
-    res.status(400).json({ message: "Missing fields" });
+    return res.status(400).json({ message: "Missing fields" });
   }
   const contact = await contacts.updateContact(id, refreshContact);
   if (contact) {
-    res.status(200).json(refreshContact);
+    return res.status(200).json(refreshContact);
   } else {
-    res.status(404).json({ message: "404 Not found!" });
+    return res.status(404).json({ message: "404 Not found!" });
   }
 });
 
-module.exports = router;
+contactsRouter.patch("/:contactId/favorite", async (req, res, next) => {
+  const { id } = req.params;
+  const { fav } = req.body;
+  const contact = await contacts.updateStatusContact(id, fav);
+  if (contact) {
+    return res.status(200);
+  } else {
+    return res.status(404).json({ message: "404 Not found!" });
+  }
+});
+
+export default contactsRouter;
